17aceb25278534054adf239430f06c9cadc50db4
egortsaryk9 <egortsaryk@gmail.com>
2018-06-04 18:30:20 +0300

    try with array

--- packages/services/deip-rpc/src/auth/serializer/src/operations.js
+++ packages/services/deip-rpc/src/auth/serializer/src/operations.js
@@ -421,7 +421,7 @@ var create_research_group = new Serializer("create_research_group", {
     description: string,
     quorum_percent: uint32,
     is_personal: bool,
-    invitees: set(invitee)
+    invitees: array(invitee)
 });
 
 var create_proposal = new Serializer("create_proposal", {
