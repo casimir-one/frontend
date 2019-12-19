a3f74d21329aa358180e857b265e3dfbfc4ad344
Euheny Bondarovich <euheny@gmail.com>
2019-12-19 14:41:33 +0300

    enable css modules

--- .storybook/config.js
+++ .storybook/config.js
@@ -29,7 +29,7 @@ addParameters({
   },
 });
 
-const req = require.context('../components', true, /\.stories\.js$/);
+const req = require.context('../packages', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,33 +1,74 @@
 const path = require('path');
 
+const moduleCssLoader = [
+    'vue-style-loader',
+    {
+        loader: 'css-loader',
+        options: {
+            url: false,
+            modules: {
+                localIdentName: '[local]_[hash:base64:4]',
+            },
+            sourceMap: true
+        }
+    }
+];
+
+const cssLoader = [
+    'vue-style-loader',
+    {
+        loader: 'css-loader',
+        options: {
+            url: false
+        }
+    }
+];
+
+const sassLoader = [
+    {
+        loader: "sass-loader",
+        options: {
+            implementation: require("sass")
+        },
+    }
+];
+
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.css$/,
-                loaders: [
-                    'style-loader',
+                oneOf: [
                     {
-                        loader: 'css-loader',
-                        options: {url: false}
+                        resourceQuery: /module/,
+                        loaders: [
+                            ...moduleCssLoader
+                        ]
+                    },
+                    {
+                        loaders: [
+                            ...cssLoader
+                        ]
                     }
                 ],
-                include: path.resolve(__dirname, '../'),
             },
             {
                 test: /\.(sass|scss)$/,
-                loaders: [
-                    'style-loader',
+                oneOf: [
                     {
-                        loader: 'css-loader',
-                        options: {url: false}
+                        resourceQuery: /module/,
+                        loaders: [
+                            ...moduleCssLoader,
+                            ...sassLoader
+                        ]
                     },
                     {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        },
-                    }],
+                        loaders: [
+                            ...cssLoader,
+                            ...sassLoader
+                        ]
+                    }
+                ],
                 include: path.resolve(__dirname, '../'),
             },
         ],
