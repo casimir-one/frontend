189d8b1fe70c790194c739ef2014fa37f907d6d7
egortsaryk9 <egortsaryk@gmail.com>
2020-05-06 04:53:08 +0300

    feat(@deip/tenant-service): Tenant signups

--- packages/services/Tenant/lib/TenantService.js
+++ packages/services/Tenant/lib/TenantService.js
@@ -4,11 +4,26 @@ import { ResearchGroupService } from '@deip/research-group-service';
 
 class TenantService extends Singleton {
   tenantHttp = TenantHttp.getInstance();
-
   researchGroupService = ResearchGroupService.getInstance();
 
   getTenantByPermlink(permlink) {
     return this.researchGroupService.getResearchGroupByPermlink(permlink);
+  }
+
+  postSignUp(data) {
+    return this.tenantHttp.postSignUp(data);
+  }
+
+  getSignUpRequests() {
+    return this.tenantHttp.getSignUpRequests();
+  }
+
+  approveSignUpRequest(username) {
+    return this.tenantHttp.approveSignUpRequest(username);
+  }
+
+  rejectSignUpRequest(username) {
+    return this.tenantHttp.rejectSignUpRequest(username);
   }
 
 }
