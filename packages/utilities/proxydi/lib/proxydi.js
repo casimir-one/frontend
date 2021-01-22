9edb533a5eccd786a6eef2470b42576eb166e6de
Euheny Bondarovich <euheny@gmail.com>
2021-01-22 09:10:00 +0300

    feat(@deip/proxydi): new utility

---
+++ packages/utilities/proxydi/lib/proxydi.js
@@ -0,0 +1,37 @@
+import { Singleton } from '@deip/toolbox';
+
+class Proxydi extends Singleton {
+  _registry = {};
+
+  get(name) {
+    return this._registry[name];
+  }
+
+  register(name, value) {
+    this._registry[name] = value;
+  }
+
+  batchRegister(registry) {
+    for (const [name, value] of Object.entries(registry)) {
+      this.register(name, value)
+    }
+  }
+
+  remove(name) {
+    delete this._registry[name]
+  }
+
+  batchRemove(names) {
+    for (const name of names) {
+      this.remove(name)
+    }
+  }
+
+  has(name) {
+    return !!this._registry[name];
+  }
+}
+
+const proxydi = Proxydi.getInstance();
+
+export { Proxydi, proxydi };
