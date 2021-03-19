04ff1dd2a53370c503870f9611c84780c1cc4a45
Euheny Bondarovich <euheny@gmail.com>
2021-03-19 14:19:54 +0300

    feat(@deip/env-module): rewrite for module resolver

--- packages/modules/env/lib/env-module.js
+++ packages/modules/env/lib/env-module.js
@@ -2,30 +2,38 @@ import axios from 'axios';
 import deipRpc from '@deip/rpc-client';
 import { proxydi } from '@deip/proxydi';
 
-export const getEnvConfig = () => {
-  return axios.get('/env')
-    .then((res) => {
-      const env = res.data;
+const init = () => axios.get('/env')
+  .then((res) => {
+    const env = res.data;
 
-      proxydi.register('env', env)
-      window.env = res.data; // TODO: temp solution
+    proxydi.register('env', env)
+    window.env = res.data; // TODO: temp solution
 
-      deipRpc.api.setOptions({
-        url: env.DEIP_FULL_NODE_URL,
-        reconnectTimeout: 3000
-      });
+    deipRpc.api.setOptions({
+      url: env.DEIP_FULL_NODE_URL,
+      reconnectTimeout: 3000
+    });
 
-      deipRpc.config.set('chain_id', env.CHAIN_ID);
+    deipRpc.config.set('chain_id', env.CHAIN_ID);
 
-      return env;
-    })
-    .then((data) => ({
-      install(Vue, options = {}) {
-        if (options.proxydi) {
-          proxydi.batchRegister(options.proxydi)
-        }
-        Vue.prototype.$env = data;
-        Vue.$env = data;
-      }
-    }));
-};
+    return env;
+  })
+
+const install = (Vue, options, envData) => {
+  if (install.installed) return;
+  install.installed = true;
+
+  if (options.proxydi) {
+    proxydi.batchRegister(options.proxydi)
+  }
+
+  Vue.prototype.$env = envData;
+  Vue.$env = envData;
+}
+
+export const EnvModule = {
+  name: 'EnvModule',
+  deps: [],
+  init,
+  install
+}
