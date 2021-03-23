c692fc1b9ad730f53eedbd5f3c49817f45296600
Euheny Bondarovich <euheny@gmail.com>
2021-03-23 15:20:16 +0300

    feat(@deip/platform-fns): add componentType helper

---
+++ packages/utilities/platform-fns/lib/utils/component-type.js
@@ -0,0 +1,12 @@
+import { pascalCase } from 'change-case';
+
+export function componentViewType() {
+  const defaultView = `${this.$options.name}${pascalCase(this.defaultViewType || 'default')}`;
+  if (this.viewType) {
+    const requestedView = `${this.$options.name}${pascalCase(this.viewType)}`;
+    const requestedViewExist = Object.keys(this.$options.components)
+      .includes(requestedView);
+    return requestedViewExist ? requestedView : defaultView;
+  }
+  return defaultView;
+}
