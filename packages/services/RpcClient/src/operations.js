4ff9013fa1d2d28b4d61c017b50aca6a22506ed1
egortsaryk9 <egortsaryk@gmail.com>
2021-04-02 19:43:41 +0200

    feat(@deip/rpc-client): Conversion to array buffer

--- packages/services/RpcClient/src/operations.js
+++ packages/services/RpcClient/src/operations.js
@@ -9,10 +9,20 @@ function createEntityOperation([op_name, op_payload], { refBlockNum, refBlockPre
   if (!serializer.entity_external_id) {
     throw new Error("External id extraction failure: 'entity_external_id' field is not specified for " + op_name);
   }
-  const buff = serializer.toEntityExternalIdBuffer(op_payload, { refBlockNum, refBlockPrefix });
-  const external_id = crypto.hexify(crypto.ripemd160(buff.buffer));
+  
+  const buff = toArrayBuffer(serializer.toEntityExternalIdBuffer(op_payload, { refBlockNum, refBlockPrefix }));
+  const external_id = crypto.hexify(crypto.ripemd160(buff));
   op_payload[serializer.entity_external_id] = external_id;
   return [external_id, [op_name, op_payload]];
+}
+
+function toArrayBuffer(buf) {
+  var ab = new ArrayBuffer(buf.length);
+  var view = new Uint8Array(ab);
+  for (var i = 0; i < buf.length; ++i) {
+    view[i] = buf[i];
+  }
+  return ab;
 }
 
 function getOperationTag(op_name) {
