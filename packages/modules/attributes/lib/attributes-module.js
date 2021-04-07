e9986fcd4d86bd7636e2e154a09dfe61a9350874
Elena Vlasova <e.vlasova@paralect.com>
2021-04-07 17:03:40 +0300

    feat(@deip/attributes-module): add store

--- packages/modules/attributes/lib/attributes-module.js
+++ packages/modules/attributes/lib/attributes-module.js
@@ -1,10 +1,18 @@
+import { proxydi } from '@deip/proxydi';
+import { attributesStore } from './store';
+
+// eslint-disable-next-line no-unused-vars
 const install = (Vue, options = {}) => {
   if (install.installed) return;
   install.installed = true;
 
-  if (!Vue.prototype.$deipModules) Vue.prototype.$deipModules = [];
-  Vue.prototype.$deipModules.push('AttributesModule');
+  const store = proxydi.get('storeInstance');
 
+  if (store) {
+    store.registerModule('attributes', attributesStore);
+  } else {
+    throw Error('[AttributesModule]: storeInstance is not provided');
+  }
 };
 
 export const AttributesModule = {
--- packages/modules/attributes/lib/attributes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = attributes;
-
-function attributes() {
-    // TODO
-}
