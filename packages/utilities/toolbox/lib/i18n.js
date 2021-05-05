1bb66cee9d2d82a00007d5feafe3365ff5bd534c
Elena Vlasova <e.vlasova@paralect.com>
2021-05-05 09:43:30 +0300

    feat(@deip/toolbox): add setLocalesMessages method

---
+++ packages/utilities/toolbox/lib/i18n.js
@@ -0,0 +1,23 @@
+import { mergeDeep } from './objects';
+
+const getMessagesByLocales = (locales) => {
+  const messages = {};
+  locales.keys().forEach((key) => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key).default;
+    }
+  });
+
+  return messages;
+};
+
+export const setLocalesMessages = (i18n, locales) => {
+  const messagesByLocales = getMessagesByLocales(locales);
+  for (const [locale, messages] of Object.entries(messagesByLocales)) {
+    const appMessages = i18n.getLocaleMessage(locale);
+    i18n.setLocaleMessage(locale, mergeDeep(appMessages, messages));
+  }
+};
