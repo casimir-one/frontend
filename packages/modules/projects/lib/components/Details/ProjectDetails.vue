9f5b24a45c7bd7919dbe8c72edc9e496f629ae6c
Elena Vlasova <e.vlasova@paralect.com>
2021-05-27 14:34:41 +0300

    chore(@deip/projects-module): add project form and details components (without attributes)

---
+++ packages/modules/projects/lib/components/Details/ProjectDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- TODO: schema renderer will be here -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProjectDetails',
+    props: {
+      project: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+</script>
