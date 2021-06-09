8ce8fe505658b6ac2b90a54e67ca1ed3a65dbb72
Euheny Bondarovich <euheny@gmail.com>
2021-06-09 12:04:45 +0300

    chore: modules updates

--- packages/services/Auth/babel.config.js
+++ packages/services/Auth/babel.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", {
-      "targets": {
-        "node": "current"
-      }
-    }]
+    '@babel/preset-env'
   ],
-  "plugins": [
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose" : true }],
-    "@babel/plugin-proposal-object-rest-spread"
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-object-rest-spread'],
+    ['@babel/plugin-proposal-optional-chaining'],
+    ['@babel/plugin-proposal-private-methods', { loose: true }]
   ]
-}
+};
