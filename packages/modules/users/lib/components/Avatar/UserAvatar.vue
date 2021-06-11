3ec1548aa1c58b106107d45a2eb3430d0508f3d2
Elena Vlasova <e.vlasova@paralect.com>
2021-06-11 15:04:17 +0200

    chore(@deip/users-module): update UserAvatar

--- packages/modules/users/lib/components/Avatar/UserAvatar.vue
+++ packages/modules/users/lib/components/Avatar/UserAvatar.vue
@@ -1,79 +1,44 @@
 <template>
-  <component
-    :is="component"
-    color="neutral lighten-5"
+  <vex-avatar
+    :src="avatarSrc"
+    :text="userInitials(user)"
     v-bind="avatarProps"
-  >
-    <img v-if="src" :src="src" alt="avatar">
-    <span
-      v-else
-      :class="[textClass, 'text--disabled']"
-    >{{ userInitials(user) }}</span>
-  </component>
+  />
 </template>
 
 <script>
-  // eslint-disable-next-line import/extensions,import/no-unresolved
-  import { VAvatar, VListItemAvatar } from 'vuetify/lib/components';
-
+  import { VexAvatar } from '@deip/vuetify-extended';
   import { getBindableProps } from '@deip/vuetify-extended/lib/composables/props';
+
   import { userHelpersMixin } from '@deip/platform-fns';
-  import { isString } from '@deip/toolbox';
 
   export default {
     name: 'UserAvatar',
+
+    components: { VexAvatar },
+
     mixins: [userHelpersMixin],
+
     props: {
-      ...VAvatar.options.props,
-      ...VListItemAvatar.options.props,
+      ...VexAvatar.props,
 
       user: {
         type: Object,
         default() { return {}; }
-      },
-
-      viewType: {
-        type: String,
-        default: 'default'
       }
     },
 
     computed: {
-      component() {
-        if (this.viewType === 'listItem') {
-          return VListItemAvatar;
-        }
-
-        return VAvatar;
-      },
-
       avatarProps() {
-        return getBindableProps.call(this, this.component.options.props);
+        return getBindableProps.call(this, VexAvatar.props);
       },
 
-      src() {
+      avatarSrc() {
         const opts = {
           width: this.$props.size,
           height: this.$props.size
         };
         return this.userAvatarSrc(this.user, opts);
-      },
-
-      sizeNumber() {
-        if (isString(this.$props.size)) {
-          return parseInt(this.$props.size.replace(/\D/g, ''), 10);
-        }
-        return this.$props.size;
-      },
-
-      textClass() {
-        if (this.sizeNumber <= 40) {
-          return 'text-caption';
-        }
-        if (this.sizeNumber <= 48) {
-          return 'text-body-2';
-        }
-        return 'text-body-1';
       }
     }
   };
