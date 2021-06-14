72996a117bee76b85c4ee55c4485ef43040815f4
Euheny Bondarovich <euheny@gmail.com>
2021-06-14 13:14:08 +0300

    feat(@deip/http-service): refactor handleError method

--- packages/services/Http/lib/HttpService.js
+++ packages/services/Http/lib/HttpService.js
@@ -2,7 +2,7 @@ import { AccessService } from '@deip/access-service';
 import axios from 'axios';
 import { proxydi } from '@deip/proxydi';
 
-import { Singleton } from '@deip/toolbox';
+import { Singleton, isFunction } from '@deip/toolbox';
 
 class HttpService extends Singleton {
   accessService = AccessService.getInstance();
@@ -12,6 +12,7 @@ class HttpService extends Singleton {
   _axiosInstance;
 
   get axios() {
+    // eslint-disable-next-line no-return-assign
     return this._axiosInstance
       ? this._axiosInstance
       : this._axiosInstance = axios.create({
@@ -23,33 +24,33 @@ class HttpService extends Singleton {
       });
   }
 
-  _handleErrors(data, status) {
-    switch (status) {
-      case 401: // sign out unauthorized user
+  _handleErrors(error) {
+    // console.error(error);
+
+    const router = proxydi.get('routerInstance');
+    const store = proxydi.get('storeInstance');
+
+    if (error.response) {
+      const { status } = error.response;
+
+      if (status === 401) {
         this.accessService.clearAccessToken();
-        window.location.replace('/sign-in');
-        break;
-      case 403: // redirect to "access denied" page
-        this.accessService.clearAccessToken();
-        window.location.replace('/sign-in');
-        break;
-      case 404: // redirect to "not-found" page
-        // router.go("/not-found");
-        break;
-      case 503: // maintenance mode
-        // router.go("/maintenance");
-        break;
-      default: // don't do anything, just pass errors to the controllers
-        console.error(data);
-        break;
+
+        if (router && store) {
+          router.push({ name: store.getters['auth/settings'].signInRedirectRouteName });
+        } else {
+          window.location.replace('/sign-in');
+        }
+
+        return false;
+        // throw new Error(data);
+      }
     }
+
+    throw error;
   }
 
-
-  _verb(method, url, model, options) {
-    if (!options) options = {};
-    const config = { ...options };
-
+  _verb(method, url, model, options = {}) {
     this.axios.defaults.headers.common.Authorization = `Bearer ${this.accessService.getAccessToken()}`;
 
     return new Promise((resolve, reject) => {
@@ -57,70 +58,36 @@ class HttpService extends Singleton {
 
       switch (method) {
         case 'get':
-          httpPromise = this.axios[method](url, config);
+          httpPromise = this.axios[method](url, options);
           break;
         case 'post':
-          httpPromise = this.axios[method](url, model, config);
+          httpPromise = this.axios[method](url, model, options);
           break;
         case 'put':
-          httpPromise = this.axios[method](url, model, config);
+          httpPromise = this.axios[method](url, model, options);
           break;
         case 'delete':
-          httpPromise = this.axios[method](url, model, config);
+          httpPromise = this.axios[method](url, model, options);
           break;
         default:
           resolve();
           break;
       }
 
+      const handleErrors = isFunction(options.handleErrors)
+        ? options.handleErrors
+        : this._handleErrors;
+
       httpPromise.then(
         (response) => {
-          // console.log(response);
           resolve(response.data);
         }
       )
-        .catch(
-          (error) => {
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              let isProcessed = false;
-              if (options && typeof (options.handleErrors) === 'function'
-                && status !== 503 /* maintenance */) {
-                isProcessed = options.handleErrors(data, status);
-              }
-              if (!isProcessed) {
-                this._handleErrors(error.response, error.response.status);
-              }
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-            } else {
-              // Something happened in setting up the request that triggered an Error
-            }
-            console.error(error);
-            reject(error);
-          }
-        );
+        .catch(handleErrors)
+        .catch((error) => {
+          reject(error);
+        });
     });
-  }
-
-  buildQueryString(obj, prefix) {
-    const str = [];
-    for (const p in obj) {
-      if (obj.hasOwnProperty(p)) {
-        const k = prefix ? `${prefix}[${p}]` : p;
-        const v = obj[p];
-
-        if (v != null && typeof v === 'object') {
-          str.push(this.buildQueryString(v, k).substr(1));
-        } else {
-          str.push(v || v === 0 || v === false ? `${k}=${encodeURIComponent(v)}` : `${k}=`);
-        }
-      }
-    }
-    return `?${str.join('&')}`;
   }
 
   get(url, options) {
