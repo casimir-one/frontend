83710429c329034e6f79b6616db92bca97ac12b9
Euheny Bondarovich <euheny@gmail.com>
2021-07-02 12:52:15 +0300

    feat(@deip/layout-service): add methods emulation

--- packages/services/Layout/lib/LayoutService.js
+++ packages/services/Layout/lib/LayoutService.js
@@ -1,4 +1,4 @@
-import { Singleton } from '@deip/toolbox';
+import { collectionMerge, genObjectId, Singleton } from '@deip/toolbox';
 import { LayoutHttp } from './LayoutHttp';
 
 class LayoutService extends Singleton {
@@ -8,10 +8,44 @@ class LayoutService extends Singleton {
     return this.layoutHttp.getLayouts();
   }
 
+  getOne(id) {
+    return this.getLayouts()
+      .then((res) => res.find((l) => l.id === id));
+  }
+
   // temp solution need change to cmd and send msg
 
   updateLayouts(data) {
     return this.layoutHttp.updateLayouts(data);
+  }
+
+  create(data) {
+    return this.getLayouts()
+      .then((res) => {
+        const entityId = genObjectId({ salt: Math.random() + new Date().getTime().toString() });
+
+        const updated = collectionMerge(res, {
+          entityId, ...data
+        }, { key: entityId });
+
+        return this.updateLayouts(updated);
+      });
+  }
+
+  update(entityId, data) {
+    return this.getLayouts()
+      .then((res) => {
+        const exist = res.find((l) => l.id === entityId);
+
+        if (!exist) {
+          throw new Error('Layout not found');
+        }
+
+        const updated = collectionMerge(res, {
+          entityId, ...data
+        }, { key: entityId });
+        return this.updateLayouts(updated);
+      });
   }
 }
 
