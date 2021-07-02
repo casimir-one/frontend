d735c7605b95492d99921b9fdf8b42e95d5f7f51
Euheny Bondarovich <euheny@gmail.com>
2021-07-02 12:47:31 +0300

    feat(@deip/attributes-service): add attrs icons

--- packages/services/Attributes/lib/constants/types.js
+++ packages/services/Attributes/lib/constants/types.js
@@ -71,7 +71,49 @@ const ATTR_TYPES_LABELS = {
   [ATTR_TYPES.FEATURE]: 'Feature attribute'
 };
 
+const ATTR_TYPES_ICONS = {
+  [ATTR_TYPES.TEXT]: 'mdi-form-textbox',
+  [ATTR_TYPES.TEXTAREA]: 'mdi-form-textarea',
+
+  [ATTR_TYPES.SELECT]: 'mdi-form-select',
+
+  [ATTR_TYPES.SWITCH]: 'mdi-toggle-switch-outline',
+  [ATTR_TYPES.CHECKBOX]: 'mdi-check-box-outline',
+
+  [ATTR_TYPES.DATE]: 'mdi-calendar',
+  [ATTR_TYPES.DATE_TIME]: 'mdi-clock-outline',
+
+  [ATTR_TYPES.FILE]: 'mdi-file-outline',
+  [ATTR_TYPES.IMAGE]: 'mdi-file-image-outline',
+  [ATTR_TYPES.URL]: 'mdi-link-variant-plus',
+  [ATTR_TYPES.NUMBER]: 'mdi-numeric',
+
+  [ATTR_TYPES.VIDEO_URL]: 'mdi-video-outline',
+  [ATTR_TYPES.USER]: 'mdi-account-outline',
+  [ATTR_TYPES.AVATAR]: 'mdi-account-circle-outline',
+  [ATTR_TYPES.LOCATION]: 'mdi-map-marker-outline',
+
+  // temp section
+
+  [ATTR_TYPES.STEPPER]: 'mdi-format-list-numbered',
+  [ATTR_TYPES.DISCIPLINE]: 'mdi-flask-empty-outline',
+  [ATTR_TYPES.RESEARCH_GROUP]: 'mdi-account-box-outline',
+  [ATTR_TYPES.EXPRESS_LICENSING]: 'mdi-file-certificate-outline',
+  [ATTR_TYPES.NETWORK_CONTENT_ACCESS]: 'mdi-account-key-outline',
+  [ATTR_TYPES.ROADMAP]: 'mdi-timeline-clock-outline',
+  [ATTR_TYPES.PARTNERS]: 'mdi-account-tie-outline',
+
+  [ATTR_TYPES.EDUCATION]: 'EDUCATION',
+  [ATTR_TYPES.EMPLOYMENT]: 'EMPLOYMENT',
+
+  // - temp section
+
+  [ATTR_TYPES.CUSTOM]: 'mdi-cogs',
+  [ATTR_TYPES.FEATURE]: 'mdi-star-circle-outline'
+};
+
 export {
   ATTR_TYPES,
-  ATTR_TYPES_LABELS
+  ATTR_TYPES_LABELS,
+  ATTR_TYPES_ICONS
 };
