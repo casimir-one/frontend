491a3d76f78157c96ea5d460ccf89a74d11ab3f4
Euheny Bondarovich <euheny@gmail.com>
2021-07-19 14:57:08 +0300

    chore: cleanup

--- packages/utilities/platform-fns/lib/helpers/index.js
+++ packages/utilities/platform-fns/lib/helpers/index.js
@@ -1,9 +1,3 @@
-export * from './user';
-export * from './team';
-export * from './tenant';
-export * from './account';
 export * from './router';
 export * from './attributes';
 export * from './roles';
-
-export * from './common';
