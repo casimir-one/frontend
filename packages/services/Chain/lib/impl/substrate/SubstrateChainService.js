2ab3aec9043a5800c33d4349d9490a2b72a6b604
egortsaryk9 <egortsaryk@gmail.com>
2021-07-21 17:23:20 +0200

    feature(@deip/chain-service): helpers methods

--- packages/services/Chain/lib/impl/substrate/SubstrateChainService.js
+++ packages/services/Chain/lib/impl/substrate/SubstrateChainService.js
@@ -9,9 +9,8 @@ import SubstrateTxBuilder from './SubstrateTxBuilder';
 import SubstrateChainApi from './SubstrateChainApi';
 import ChainTypes from './ChainTypes';
 import { Metadata } from '@polkadot/metadata';
-import { u8aToHex, isU8a } from '@polkadot/util';
-import { encodeAddress } from '@polkadot/util-crypto';
-
+import { u8aToHex, hexToU8a, isU8a, isHex } from '@polkadot/util';
+import { encodeAddress, decodeAddress, blake2AsU8a, createKeyMulti } from '@polkadot/util-crypto';
 
 class SubstrateChainService extends BaseChainService {
 
@@ -31,7 +30,11 @@ class SubstrateChainService extends BaseChainService {
       return ApiPromise.create({ provider, registry: typesRegistry })
         .then((chainNodeClient) => {
           this._chainNodeClient = chainNodeClient;
-          this._chainOpsRegistry = new SubstrateChainOperationsRegistry(this._chainNodeClient);
+          this._chainOpsRegistry = new SubstrateChainOperationsRegistry(this._chainNodeClient, {
+            pubKeyToAddress: this.pubKeyToAddress.bind(this),
+            daoIdToAddress: this.daoIdToAddress.bind(this),
+            isAddress: this.isAddress.bind(this)
+          });
           this._chainApi = new SubstrateChainApi(this);
 
           return Promise.all([
@@ -65,6 +68,34 @@ class SubstrateChainService extends BaseChainService {
   pubKeyToAddress(pubKey, addressFormat = 42) {
     const address = encodeAddress(isU8a(pubKey) ? u8aToHex(pubKey) : pubKey, addressFormat);
     return address;
+  }
+
+  daoIdToAddress(daoId) {
+    const H160 = this._chainNodeClient.registry.createType('H160', daoId);
+    const VecU8 = this._chainNodeClient.registry.createType('Vec<u8>', H160);
+    const scaleVecU8 = VecU8.toU8a();
+    const hash = blake2AsU8a(scaleVecU8, 256);
+    const pubKey = this._chainNodeClient.registry.createType('AccountId', hash);
+    return this.pubKeyToAddress(pubKey);
+  }
+
+  isAddress(address, addressFormat) {
+    try {
+      encodeAddress(
+        isHex(address)
+          ? hexToU8a(address)
+          : decodeAddress(address, false, addressFormat),
+        addressFormat
+      );
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  getMultiAddress(addresses, threshold) {
+    const multiAddress = createKeyMulti([...addresses].sort(), threshold);
+    return multiAddress;
   }
 
 }
