87cf7e19891cd1bc95bb5b726099cf578a7117d2
Elena Vlasova <e.vlasova@paralect.com>
2021-07-26 09:53:06 +0200

    feat(@deip/platfrom-fns): remove methods

--- packages/utilities/platform-fns/lib/helpers/roles.js
+++
@@ -1,7 +0,0 @@
-import { intersectionWith } from 'lodash/fp';
-import { wrapInArray } from '@deip/toolbox';
-
-export const hasRoles = (userRoles, roles) => {
-  const rolesList = wrapInArray(roles);
-  return intersectionWith((uR, rR) => uR.role === rR, userRoles, rolesList).length > 0;
-};
