b593e48ece058a2e1bb8b508f583bc7db6cd4aab
Euheny Bondarovich <euheny@gmail.com>
2021-07-29 17:25:47 +0300

    chore(@deip/teams-module): add canEdit to schemaData

--- packages/modules/teams/lib/components/TeamDetails.vue
+++ packages/modules/teams/lib/components/TeamDetails.vue
@@ -9,6 +9,7 @@
 <script>
   import { attributedDetailsFactory, LayoutRenderer } from '@deip/layouts-module';
   import { attributeMethodsFactory, expandAttributes } from '@deip/platform-fns';
+  import { SYSTEM_ROLE } from '@deip/constants';
 
   export default {
     name: 'TeamDetails',
@@ -24,6 +25,10 @@
     computed: {
       schemaData() {
         return {
+          canEdit: this.$currentUser.hasRole(
+            SYSTEM_ROLE.TEAM_ADMIN,
+            { name: 'teamId', id: this.team.entityId }
+          ),
           ...attributeMethodsFactory(
             expandAttributes(this.team),
             {
