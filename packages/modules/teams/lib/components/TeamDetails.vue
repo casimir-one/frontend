233888007ce27fe4c9d3438b91245b58b14f794e
Euheny Bondarovich <euheny@gmail.com>
2021-07-29 15:30:57 +0300

    chore(@deip/teams-module): switch to layout-renderer

--- packages/modules/teams/lib/components/TeamDetails.vue
+++ packages/modules/teams/lib/components/TeamDetails.vue
@@ -1,22 +1,20 @@
 <template>
-  <schema-renderer
+  <layout-renderer
     :value="team"
     :schema="internalSchema"
-    :components="internalComponents"
     :schema-data="schemaData"
   />
 </template>
 
 <script>
-  import { SchemaRenderer } from '@deip/schema-renderer';
-  import { attributedDetailsFactory } from '@deip/layouts-module';
+  import { attributedDetailsFactory, LayoutRenderer } from '@deip/layouts-module';
   import { attributeMethodsFactory, expandAttributes } from '@deip/platform-fns';
 
   export default {
     name: 'TeamDetails',
 
     components: {
-      SchemaRenderer
+      LayoutRenderer
     },
 
     mixins: [
