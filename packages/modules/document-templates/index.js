c4bcc8a2b1d9a237afa054aa2eefb98b73681037
Elena Vlasova <e.vlasova@paralect.com>
2021-08-16 23:03:15 +0200

    feat(@deip/document-templates-module): add document templates table component

--- packages/modules/document-templates/index.js
+++ packages/modules/document-templates/index.js
@@ -1,1 +1,4 @@
 export * from './lib/document-templates-module';
+
+export { default as DocumentTemplatesList } from './lib/components/List/DocumentTemplatesList';
+export { default as DocumentTemplatesDataProvider } from './lib/components/DataProvider/DocumentTemplatesDataProvider';
--- packages/modules/document-templates/lib/document-templates-module.js
+++ packages/modules/document-templates/lib/document-templates-module.js
@@ -1,11 +1,21 @@
 import { proxydi } from '@deip/proxydi';
+import { setLocalesMessages } from '@deip/toolbox';
 import { documentTemplatesStore } from './store';
+
+const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.js$/i);
 
 const install = () => {
   if (install.installed) return;
   install.installed = true;
 
   const store = proxydi.get('storeInstance');
+  const i18n = proxydi.get('i18nInstance');
+
+  if (i18n) {
+    setLocalesMessages(i18n, locales);
+  } else {
+    throw Error('[DocumentTemplatesModule]: i18nInstance is not provided');
+  }
 
   if (store) {
     store.registerModule('documentTemplates', documentTemplatesStore);
