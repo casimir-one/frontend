5f55f3b960bc07f73010ec216a6a9dd93f0c85e2
Elena Vlasova <e.vlasova@paralect.com>
2021-08-17 18:33:45 +0200

    feat(@deip/document-templates-module): add template form component

--- packages/modules/document-templates/lib/locales/en.js
+++ packages/modules/document-templates/lib/locales/en.js
@@ -4,6 +4,14 @@ export default {
       list: {
         title: 'Title',
         created: 'Created'
+      },
+      form: {
+        title: 'Title',
+        bodyPlaceholder: 'Start typing here...',
+        cancel: 'Cancel',
+        create: 'Create',
+        update: 'Update',
+        placeholdersMessage: 'You can use placeholders for automatic data fill.'
       }
     }
   }
