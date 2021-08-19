dac9149211822802cc02a7b161ca14111c222e8c
Elena Vlasova <e.vlasova@paralect.com>
2021-08-19 17:37:17 +0200

    feat(@deip/document-templates-module): add document template details component

--- packages/modules/document-templates/index.js
+++ packages/modules/document-templates/index.js
@@ -3,3 +3,4 @@ export * from './lib/document-templates-module';
 export { default as DocumentTemplatesList } from './lib/components/List/DocumentTemplatesList';
 export { default as DocumentTemplatesDataProvider } from './lib/components/DataProvider/DocumentTemplatesDataProvider';
 export { default as DocumentTemplateForm } from './lib/components/Form/DocumentTemplateForm';
+export { default as DocumentTemplateDetails } from './lib/components/Details/DocumentTemplateDetails';
