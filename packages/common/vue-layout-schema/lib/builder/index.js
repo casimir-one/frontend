1fd6622739290e19e846f297df2faf84134ce3a9
Euheny Bondarovich <euheny@gmail.com>
2021-09-28 14:20:36 +0300

    chore: cleanup

--- packages/common/vue-layout-schema/lib/builder/index.js
+++ packages/common/vue-layout-schema/lib/builder/index.js
@@ -4,8 +4,3 @@ export { VlsBuilderCanvasTree } from './components/canvas-tree';
 export { VlsBuilderBlockSettings } from './components/block-settings';
 
 export * from './utils/helpers';
-
-// export { SchemaBuilderBlocksList } from './components/blocks-list';
-// export { SchemaBuilderCanvas } from './components/canvas';
-// export { SchemaBuilderCanvasTree } from './components/canvas-tree';
-// export { SchemaBuilderBlockSettings } from './components/block-settings';
