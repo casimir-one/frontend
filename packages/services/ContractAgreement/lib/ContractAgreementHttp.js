4e7265531e197a8af72779a7667e89f3b2d01837
Egor Anikey <egor.anikey@deip.world>
2021-10-04 17:32:00 +0300

    feat(@deip/contract-agreement-service): add routes

--- packages/services/ContractAgreement/lib/ContractAgreementHttp.js
+++ packages/services/ContractAgreement/lib/ContractAgreementHttp.js
@@ -1,5 +1,6 @@
 import { HttpService } from '@deip/http-service';
 import { Singleton } from '@deip/toolbox';
+import qs from 'qs';
 
 class ContractAgreementHttp extends Singleton {
   http = HttpService.getInstance();
@@ -16,40 +17,17 @@ class ContractAgreementHttp extends Singleton {
     return this.http.post('/api/v2/contract-agreement', req.getHttpBody());
   }
 
-  getProjectLicense(licenseId) {
-    return this.http.get(`/api/v2/contract-agreement/license/${licenseId}`);
-  }
-
-  getProjectLicensesByLicensee(licensee) {
-    return this.http.get(`/api/v2/contract-agreement/licenses/licensee/${licensee}`);
-  }
-
-  getProjectLicensesByLicenser(licenser) {
-    return this.http.get(`/api/v2/contract-agreement/licenses/licenser/${licenser}`);
-  }
-
-  getProjectLicensesByProject(projectId) {
-    return this.http.get(`/api/v2/contract-agreement/licenses/projectId/${projectId}`);
-  }
-
-  getProjectLicensesByLicenseeAndProject(licensee, projectId) {
-    return this.http.get(`/api/v2/contract-agreement/licenses/licensee/${licensee}/projectId/${projectId}`);
-  }
-
-  getProjectLicensesByLicenseeAndLicenser(licensee, licenser) {
-    return this.http.get(`/api/v2/contract-agreement/licenses/licensee/${licensee}/licenser/${licenser}`);
-  }
-
   getContractAgreementsListByCreator(creator) {
     return this.http.get(`/api/v2/contract-agreements/creator/${creator}`);
   }
 
-  getIncomeShareAgreement(incomeShareAgreementId) {
-    return this.http.get(`/api/v2/contract-agreement/isa/${incomeShareAgreementId}`);
+  getContractAgreements(query) {
+    const q = qs.stringify({ ...query });
+    return this.http.get(`/api/v2/contract-agreements?${q}`);
   }
 
-  getIncomeShareAgreementsListByCreator(creator) {
-    return this.http.get(`/api/v2/contract-agreement/isas/creator/${creator}`);
+  getContractAgreement(contractAgreementId) {
+    return this.http.get(`/api/v2/contract-agreement/${contractAgreementId}`);
   }
 }
 
