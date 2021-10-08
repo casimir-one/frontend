7d5fd2e295d8bffd39c63847c778549a19c56275
Elena Vlasova <e.vlasova@paralect.com>
2021-10-08 16:29:17 +0200

    feat(@deip/contract-agreements-module): add ContractPdfDetails

--- packages/modules/contract-agreements/index.js
+++ packages/modules/contract-agreements/index.js
@@ -2,3 +2,4 @@ export * from './lib/contract-agreements-module';
 
 export { default as ContractAgreementsDataProvider } from './lib/components/DataProvider/ContractAgreementsDataProvider';
 export { default as ContractsTable } from './lib/components/Table/ContractsTable';
+export { default as ContractPdfDetails } from './lib/components/Details/ContractPdfDetails';
