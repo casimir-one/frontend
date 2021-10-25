bdd32e924288f07360c0e61b797c2f4bd5d0fd45
VadimSVB <zoomvrum.vs@gmail.com>
2021-10-25 13:38:33 +0300

    feat(@deip/auth-module):add change password component
    - Module for changing the password
    
    - Added new translation options for the authentication module
    
    - Added a new form validation rule: equal
    
    - Created a new Vex component VexPasswordRepeatInput for entering a new password and repeating it
    
    - Fixed recursion problem in 'toolbox/lib/files'

---
+++ packages/plugins/VuetifyExtended/lib/components/VexInput/VexPasswordRepeatInput/VexPasswordRepeatInput.vue
@@ -0,0 +1,98 @@
+<template>
+  <vex-stack :gutter="8" class="mb-7">
+    <!-- new password -->
+
+    <validation-provider
+      v-slot="{ errors }"
+      :name="passwordLabel"
+      :rules="{
+        required: true,
+        minMax: { min: passwordMinLentgh, max: passwordMaxLentgh }}"
+    >
+      <vex-password-input
+        v-model="internalPassword"
+        :label="passwordLabel"
+        :error-messages="errors"
+        v-bind="fieldsProps"
+      />
+    </validation-provider>
+
+    <!-- repeat new password -->
+
+    <validation-provider
+      v-slot="{ errors }"
+      :name="repeatPasswordLabel"
+      :rules="{
+        required: true,
+        minMax: { min: passwordMinLentgh, max: passwordMaxLentgh },
+        equal: { target: internalPassword, label:passwordLabel },
+      }"
+    >
+      <vex-password-input
+        v-model="repeatePassword"
+        :label="repeatPasswordLabel"
+        :error-messages="errors"
+        v-bind="fieldsProps"
+      />
+    </validation-provider>
+  </vex-stack>
+</template>
+<script>
+  import { VexStack } from '../../VexStack';
+  import { VexPasswordInput } from '../VexPasswordInput';
+
+  export default {
+    name: 'VexPasswordRepeatInput',
+    components: {
+      VexStack,
+      VexPasswordInput
+    },
+    model: {
+      prop: 'password',
+      event: 'change'
+    },
+    props: {
+      passwordLabel: {
+        type: String,
+        default: 'Password'
+      },
+      repeatPasswordLabel: {
+        type: String,
+        default: 'Repeat password'
+      },
+      fieldsProps: {
+        type: Object,
+        default: () => ({
+          counter: true
+        })
+      },
+      passwordMinLentgh: {
+        type: Number,
+        default: 10
+      },
+      passwordMaxLentgh: {
+        type: Number,
+        default: 64
+      },
+      password: {
+        type: String,
+        default: ''
+      }
+    },
+    data() {
+      return {
+        repeatePassword: ''
+      };
+    },
+    computed: {
+      internalPassword: {
+        get() {
+          return this.password;
+        },
+        set(value) {
+          this.$emit('change', value);
+        }
+      }
+    }
+  };
+</script>
