3bacfa682495d2092fbfbf5a50600603f4781da5
Elena Vlasova <e.vlasova@paralect.com>
2021-11-30 09:59:16 +0100

    test(@deip/toolbox): add tests

---
+++ packages/common/toolbox/__tests__/strings.test.js
@@ -0,0 +1,19 @@
+import { convertToUnit } from '../lib/strings';
+
+describe('convertToUnit', () => {
+  it.each([null, undefined, '', ' '])('should return undefined for %s', (initial) => {
+    expect(convertToUnit(initial)).toBeUndefined();
+  });
+
+  it.each(['hello', '1,8', true])('should return initial string %s', (initial) => {
+    expect(convertToUnit(initial)).toBe(String(initial));
+  });
+
+  it.each([['1', '1px'], [-2, '-2px'], ['2.5', '2.5px']])('should return $expected for $initial', (initial, expected) => {
+    expect(convertToUnit(initial)).toBe(expected);
+  });
+
+  it('should add custom unit to result', () => {
+    expect(convertToUnit(2, 'pt')).toBe('2pt');
+  });
+});
