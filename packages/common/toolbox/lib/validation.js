72bcb031ca0df9cc2cdded9ecccb0131f49d920e
Euheny Bondarovich <euheny@gmail.com>
2021-12-10 09:41:46 +0300

    chore(@deip/toolbox): add jsdoc and refact

--- packages/common/toolbox/lib/validation.js
+++ packages/common/toolbox/lib/validation.js
@@ -81,7 +81,15 @@ export const hasOwnProperty = (prop, obj) => {
 
  validateCondition(cond, 'alarma!!!')
 */
-export const assert = (condition, failureMessage = 'Assertion failed') => {
+
+/**
+ * @param {boolean} condition
+ * @param {string} [failureMessage = Assertion failed]
+ */
+export const assert = (
+  condition,
+  failureMessage = 'Assertion failed',
+) => {
   if (condition) return;
   throw new Error(failureMessage);
 };
