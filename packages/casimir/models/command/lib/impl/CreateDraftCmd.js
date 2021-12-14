e71032debe5218b4ed380f41bffc0e56234f103e
egor-anikey <egor.anekey@deip.world>
2021-12-14 13:00:03 +0300

    feat: JSON type in pojectContent

--- packages/casimir/models/command/lib/impl/CreateDraftCmd.js
+++ packages/casimir/models/command/lib/impl/CreateDraftCmd.js
@@ -1,5 +1,5 @@
 import AppCmd from '../base/AppCmd';
-import { APP_CMD } from '@deip/constants';
+import { APP_CMD, PROJECT_CONTENT_FORMAT } from '@deip/constants';
 import { assert } from '@deip/toolbox';
 
 class CreateDraftCmd extends AppCmd {
@@ -9,11 +9,15 @@ class CreateDraftCmd extends AppCmd {
     const {
       projectId,
       formatType,
-      draftId
+      draftId,
+      jsonData
     } = cmdPayload;
 
     assert(!!projectId, "'projectId' is required");
     assert(!!formatType, "'formatType' is required");
+    if (formatType === PROJECT_CONTENT_FORMAT.JSON) {
+      assert(!!jsonData, `'jsonData' is required for ${formatType} formatType`);
+    }
     assert(!!draftId, "'draftId' is required");
 
     super(APP_CMD.CREATE_DRAFT, cmdPayload);
