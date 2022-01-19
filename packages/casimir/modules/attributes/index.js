62fedb9c2d4c799ef6301024f0d72c4115dafd49
Stacey Korbit <nastyasworld@gmail.com>
2022-01-19 23:56:42 +0300

    refactor(@deip/attributes-module): add attributes list to components

--- packages/casimir/modules/attributes/index.js
+++ packages/casimir/modules/attributes/index.js
@@ -5,3 +5,4 @@ export { default as AttributeSet } from './lib/components/AttributeSet';
 export { default as AttributeRead } from './lib/components/AttributeRead';
 
 export { default as AttributesSetup } from './lib/components/AttributesSetup';
+export { AttributesList } from './lib/components/AttributesList';
---
+++ packages/casimir/modules/attributes/lib/components/AttributesList/AttributesList.vue
@@ -0,0 +1,244 @@
+<template>
+  <v-sheet>
+    <v-sheet color="neutral lighten-5" rounded class="pa-6">
+      <v-row>
+        <v-col cols="2">
+          <v-select
+            v-model.number="filter.scope"
+            label="Attributes scope"
+            :items="scopesItems"
+            outlined
+            hide-details
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-model="filter.search"
+            label="Search"
+            outlined
+            hide-details
+          />
+        </v-col>
+      </v-row>
+    </v-sheet>
+
+    <v-data-table
+      :items="attributes"
+      :headers="headers"
+
+      :hide-default-footer="attributes.length < itemsPerPage"
+      :footer-props="{itemsPerPageOptions: [5, 10, 20, 50, -1]}"
+      :items-per-page="itemsPerPage"
+
+      :search="filter.search"
+    >
+      <template #item.scope="{ item }">
+        <v-chip
+          small
+          outlined
+          readonly
+          :color="scopeColors[item.scope].background"
+        >
+          {{ ATTR_SCOPES_LABELS[item.scope] }}
+        </v-chip>
+      </template>
+
+      <template #item.type="{ item }">
+        <v-chip small outlined readonly>
+          {{ ATTR_TYPES_LABELS[item.type] }}
+        </v-chip>
+      </template>
+
+      <template #item.markers="{ item }">
+        <!--          'isEditable',-->
+        <!--          'isFilterable',-->
+        <!--          'isHidden',-->
+        <!--          'isRequired',-->
+        <!--          'isSystem'-->
+
+        <ve-stack flow="column" :gap="8" class="justify-end">
+          <vex-tooltip v-if="!item.isEditable" top tooltip="Not editable after first fill">
+            <v-icon small class="text--disabled">
+              mdi-file-cancel-outline
+            </v-icon>
+          </vex-tooltip>
+
+          <vex-tooltip v-if="item.isFilterable" top tooltip="Used in filter">
+            <v-icon small class="text--disabled">
+              mdi-filter-variant
+            </v-icon>
+          </vex-tooltip>
+
+          <vex-tooltip v-if="item.isHidden" top tooltip="Hidden field">
+            <v-icon small class="text--disabled">
+              mdi-eye-off-outline
+            </v-icon>
+          </vex-tooltip>
+
+          <vex-tooltip v-if="item.isRequired" top tooltip="Required field">
+            <v-icon small class="text--disabled">
+              mdi-asterisk
+            </v-icon>
+          </vex-tooltip>
+          <!-- <vex-tooltip v-if="item.blockchainFieldMeta" top tooltip="Form required.">-->
+          <!-- <v-icon small class="text&#45;&#45;disabled">-->
+          <!-- mdi-shield-alert-outline-->
+          <!-- </v-icon>-->
+          <!-- </vex-tooltip>-->
+
+          <vex-tooltip v-if="item.isSystem" top tooltip="System field.">
+            <v-icon small class="text--disabled">
+              mdi-shield-alert-outline
+            </v-icon>
+          </vex-tooltip>
+        </ve-stack>
+      </template>
+
+      <template #item.actions="{ item }">
+        <ve-stack flow="column" :gap="8" class="justify-end">
+          <v-btn
+            icon
+            x-small
+            @click="onClickEdit(item)"
+          >
+            <v-icon
+              small
+            >
+              mdi-pencil
+            </v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            x-small
+            :disabled="item.isSystem"
+            @click="onClickRemove(item._id)"
+          >
+            <v-icon
+              small
+            >
+              mdi-delete
+            </v-icon>
+          </v-btn>
+        </ve-stack>
+      </template>
+    </v-data-table>
+  </v-sheet>
+</template>
+
+<script>
+  // TODO: switch to prototype instance
+  import { ATTR_SCOPES, ATTR_SCOPES_LABELS, ATTR_TYPES_LABELS } from '@deip/constants';
+  import { genColorsPalette } from '@deip/toolbox';
+
+  import {
+
+    VexTooltip
+  } from '@deip/vuetify-extended';
+  import { VeStack } from '@deip/vue-elements';
+
+  const scopeColors = genColorsPalette({
+    palette: ['#FFC255', '#FF8863', '#FF5484', '#CD3DA9', '#6846C0'],
+    colorsCount: ATTR_SCOPES.values().length
+  }).reduce((acc, color, index) => ({
+    ...acc,
+    ...{ [ATTR_SCOPES.values()[index]]: color }
+  }), {});
+
+  export default {
+    name: 'AttributesList',
+
+    components: {
+      VeStack,
+      VexTooltip
+    },
+
+    props: {
+      onClickEdit: {
+        type: Function,
+        default: () => {}
+      },
+      onClickRemove: {
+        type: Function,
+        default: () => {}
+      }
+    },
+
+    data() {
+      return {
+        ATTR_SCOPES,
+        ATTR_SCOPES_LABELS,
+        ATTR_TYPES_LABELS,
+
+        filter: {
+          scope: 0,
+          search: ''
+        },
+
+        scopeColors,
+
+        itemsPerPage: 50,
+
+        scopesItems: [
+          { value: 0, text: 'All' },
+          ...ATTR_SCOPES.values().map((scope) => ({
+            value: scope,
+            text: ATTR_SCOPES_LABELS[scope]
+          }))
+        ]
+      };
+    },
+
+    computed: {
+      attributes() {
+        return this.$store.getters['attributes/list'](
+          this.filter.scope
+            ? { scope: this.filter.scope }
+            : {}
+        );
+      },
+
+      headers() {
+        return [
+          ...(!this.filter.scope ? [{
+            text: 'Scope',
+            value: 'scope',
+            width: 1
+          }] : []),
+          {
+            text: 'Type',
+            value: 'type',
+            width: 1
+          },
+
+          {
+            text: 'Title',
+            align: 'start',
+            sortable: true,
+            value: 'title'
+          },
+          {
+            text: 'Short title',
+            align: 'start',
+            sortable: true,
+            value: 'shortTitle'
+          },
+          {
+            align: 'start',
+            sortable: false,
+            value: 'markers'
+          },
+          {
+            align: 'end',
+            sortable: false,
+            value: 'actions'
+          }
+        ];
+      }
+    },
+
+    created() {
+      this.$store.dispatch('attributes/getList');
+    }
+
+  };
+</script>
