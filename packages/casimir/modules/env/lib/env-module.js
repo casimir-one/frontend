50ee55c2cca3e7ce48e4698a7167b0c12d4a4729
Euheny Bondarovich <euheny@gmail.com>
2022-01-28 15:43:05 +0300

    chore(@deip/env-module): add i18n locale setup

--- packages/casimir/modules/env/lib/env-module.js
+++ packages/casimir/modules/env/lib/env-module.js
@@ -3,6 +3,9 @@ import { proxydi } from '@deip/proxydi';
 import { ChainService } from '@deip/chain-service';
 
 import Clipboard from 'v-clipboard';
+import { assert } from '@deip/toolbox';
+
+const moduleName = 'EnvModule';
 
 const init = async () => {
   const { data: env } = await axios.get('/env');
@@ -42,11 +45,21 @@ const install = (Vue, options, envData) => {
   Vue.prototype.$env = envData;
   Vue.$env = envData;
 
+  assert(!!i18n, `[${moduleName}]: i18n instance is not provided`);
+
+  const {
+    VUE_APP_I18N_LOCALE: locale,
+    VUE_APP_I18N_FALLBACK_LOCALE: fallbackLocale
+  } = envData;
+
+  if (locale) i18n.locale = locale;
+  if (fallbackLocale) i18n.fallbackLocale = fallbackLocale;
+
   Vue.use(Clipboard);
 };
 
 export const EnvModule = {
-  name: 'EnvModule',
+  name: moduleName,
   deps: [],
   init,
   install
