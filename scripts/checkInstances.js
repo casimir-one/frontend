eddd8d8ae6597ca9b7252ed26dab14a99726a987
Elena Vlasova <e.vlasova@paralect.com>
2022-02-04 14:16:40 +0100

    chore: add check:instances script

---
+++ scripts/checkInstances.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import glob from 'glob';
+import path from 'path';
+import fs from 'fs-extra';
+/* eslint-enable */
+import { getPackages } from './composables/getPackages';
+
+const pkg = getPackages(false);
+
+const result = pkg.reduce((acc, p) => {
+  const { name } = fs.readJsonSync(path.join(p, 'package.json'));
+  const instances = glob.sync(path.join(p, 'node_modules', '+(vue|vuetify|vuex)'))
+    .map((inst) => path.parse(inst).base);
+  const res = {
+    [name]: instances
+  };
+
+  return { ...acc, ...(instances.length ? res : {}) };
+}, {});
+
+console.info(result);
--- scripts/composables/getPackages.js
+++ scripts/composables/getPackages.js
@@ -12,12 +12,16 @@ const lernaConf = fs.readJsonSync(path.join(rootPath, 'lerna.json'));
 /**
  * @return {Array} list of packages
  */
-const getPackages = () => lernaConf.packages
+const getPackages = (forBuild = true) => lernaConf.packages
   .filter((pattern) => !pattern.includes('generators'))
   .reduce((acc, pattern) => [...acc, ...glob.sync(pattern, { absolute: true })], [])
   .filter((p) => {
     const pArr = path.parse(p);
-    return fs.existsSync(path.join(p, 'src')) && !legacyExcludes.includes(pArr.name);
+    if (forBuild) {
+      return fs.existsSync(path.join(p, 'src')) && !legacyExcludes.includes(pArr.name);
+    }
+
+    return true;
   });
 
 export {
