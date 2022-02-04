f4dc15e85ee576b4610e2afba02ab31a55192431
Elena Vlasova <e.vlasova@paralect.com>
2022-02-04 13:51:08 +0100

    refactor(@deip/platform-util): refactor create app

--- packages/casimir/platform/util/lib/common/CreateApp.js
+++ packages/casimir/platform/util/lib/common/CreateApp.js
@@ -1,7 +1,7 @@
 export class CreateApp {
-  _registeredModules = {}
+  #registeredModules = {}
 
-  _modulesStorage = {}
+  #modulesStorage = {}
 
   /**
    * @param {*} vueInstance
@@ -10,6 +10,55 @@ export class CreateApp {
   constructor(vueInstance, provideOptions = {}) {
     this.Vue = vueInstance;
     this.provideOptions = provideOptions;
+  }
+
+  /**
+   * @param {string} name
+   * @return {Promise|Function}
+   */
+  #installModule(name) {
+    if (this.#modulesStorage[name]) {
+      return this.#modulesStorage[name];
+    }
+
+    const { deps = [] } = this.#registeredModules[name].module;
+
+    if (!deps.every((dep) => this.#registeredModules[dep])) {
+      const missed = deps.filter((dep) => !this.#registeredModules[dep]);
+      throw Error(`[${missed.join(', ')}] deps missed for ${name}`);
+    }
+
+    const useFn = () => {
+      const {
+        module,
+        options
+      } = this.#registeredModules[name];
+
+      const {
+        injectOptions = true
+      } = options;
+
+      return (
+        module.init
+          ? module.init()
+          : Promise.resolve()
+      ).then((data) => {
+        const mergedOpts = {
+          ...(injectOptions ? this.provideOptions : {}),
+          ...options
+        };
+        this.Vue.use(module, mergedOpts, data);
+      });
+    };
+
+    this.#modulesStorage[name] = (
+      deps.length === 0
+        ? Promise.resolve()
+        : Promise.all(deps.map((dep) => this.#installModule(dep)))
+    )
+      .then(useFn);
+
+    return this.#modulesStorage[name];
   }
 
   /**
@@ -24,7 +73,7 @@ export class CreateApp {
       throw Error('Module name not provided');
     }
 
-    this._registeredModules[name] = {
+    this.#registeredModules[name] = {
       module,
       options
     };
@@ -33,58 +82,18 @@ export class CreateApp {
   }
 
   /**
-   * @param {string} name
-   * @return {Promise|Function}
-   */
-  installModule(name) {
-    if (this._modulesStorage[name]) {
-      return this._modulesStorage[name];
-    }
-
-    const { deps = [] } = this._registeredModules[name].module;
-
-    if (!deps.every((dep) => this._registeredModules[dep])) {
-      const missed = deps.filter((dep) => !this._registeredModules[dep]);
-      throw Error(`[${missed.join(', ')}] deps missed for ${name}`);
-    }
-
-    const useFn = () => {
-      const {
-        module,
-        options
-      } = this._registeredModules[name];
-
-      return (
-        module.init
-          ? module.init()
-          : Promise.resolve())
-
-        .then((data) => {
-          const mergedOpts = {
-            ...(options.injectOptions !== false ? this.provideOptions : {}),
-            ...options
-          };
-          this.Vue.use(module, mergedOpts, data);
-        });
-    };
-
-    this._modulesStorage[name] = (
-      deps.length === 0
-        ? Promise.resolve()
-        : Promise.all(deps.map((dep) => this.installModule(dep)))
-    )
-      .then(useFn);
-
-    return this._modulesStorage[name];
-  }
-
-  /**
    * @return {Array.<Promise>}
    */
-  bootstrap() {
-    return Promise.all(
-      Object.keys(this._registeredModules)
-        .map((module) => this.installModule(module))
+  async bootstrap() {
+    await Promise.all(
+      Object.keys(this.#registeredModules)
+        .map((module) => this.#installModule(module))
     );
+
+    const installed = Object.keys(this.#registeredModules).sort();
+
+    this.Vue.prototype.$casimirModules = installed;
+
+    return installed;
   }
 }
