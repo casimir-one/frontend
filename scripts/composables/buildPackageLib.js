6aedcf343bb64b68534f182d49a9184bdd626a91
Euheny Bondarovich <euheny@gmail.com>
2022-02-18 12:00:33 +0300

    chore: change lib builder

--- scripts/composables/buildPackageLib.js
+++ scripts/composables/buildPackageLib.js
@@ -2,25 +2,24 @@
 import { parseComponent } from 'vue-template-compiler';
 import fs from 'fs-extra';
 import { transformSync } from '@babel/core';
-import shell from 'shelljs';
 import path from 'path';
 import glob from 'glob';
-import ora from 'ora';
-import chalk from 'chalk';
 /* eslint-enable */
 
-import { getAttrsString, changePathToLib } from './utils';
+import { getAttrsString, changePathToLib, asyncExec } from './utils';
 import { babelConfPath } from './paths';
 
-const spinner = ora();
 const scriptExtensions = ['js', 'ts', 'tsx'];
 
 /**
  * @param {string} pkgPath
+ * @return {Promise<void>}
  */
-const processVue = (pkgPath) => {
+const processVue = async (pkgPath) => {
   const pattern = path.join(pkgPath, 'src', '**', '*.vue');
   const files = glob.sync(pattern);
+
+  const operations = [];
 
   for (const file of files) {
     const { template, script, styles } = parseComponent(fs.readFileSync(file, 'utf8'));
@@ -49,55 +48,103 @@ const processVue = (pkgPath) => {
       ...processedStyles
     ].join('\n\n');
 
-    try {
-      fs.outputFileSync(
-        changePathToLib(pkgPath, file),
-        processedResult
-      );
-    } catch (err) {
-      console.error(err);
-    }
+    operations.push(fs.outputFile(
+      changePathToLib(pkgPath, file),
+      processedResult
+    ));
+  }
+
+  try {
+    await Promise.all(operations);
+  } catch (err) {
+    console.error(err);
   }
 };
 
 /**
  * @param {string} pkgPath
+ * @return {Promise<void>}
  */
-const processScripts = (pkgPath) => {
-  const babelExt = scriptExtensions.map((ext) => `.${ext}`).join(',');
-  shell.exec(
-    `cross-env NODE_ENV=lib babel --config-file ${babelConfPath} ${pkgPath}/src --out-dir ${pkgPath}/lib --extensions "${babelExt}"`,
-    { silent: true }
-  );
-
-  shell.exec(
-    `tsc ${pkgPath}/src/*.ts --declaration --emitDeclarationOnly --outFile ${pkgPath}/lib/index.d.ts`,
-    { silent: true }
-  );
+const cleanLib = async (pkgPath) => {
+  await asyncExec(`shx rm -rf ${pkgPath}/lib`);
 };
 
 /**
  * @param {string} pkgPath
+ * @return {Promise<void>}
  */
-const processOtherFiles = (pkgPath) => {
+const lintLib = async (pkgPath) => {
+  await asyncExec(`eslint --fix ${pkgPath}/lib`);
+};
+
+/**
+ * @param {string} pkgPath
+ * @return {Promise<void>}
+ */
+const generateTsDefinitions = async (pkgPath) => {
+  const tscCommandStack = [
+    `-p ${pkgPath}/tsconfig.build.json`,
+    '--emitDeclarationOnly',
+    `--outFile ${pkgPath}/lib/types.d.ts`
+  ].join(' ');
+
+  await asyncExec(`tsc ${tscCommandStack}`, { silent: true });
+};
+
+/**
+ * @param {string} pkgPath
+ * @return {Promise<void>}
+ */
+const processScripts = async (pkgPath) => {
+  const babelExt = scriptExtensions.map((ext) => `.${ext}`).join(',');
+
+  const babelCommandStack = [
+    `--config-file ${babelConfPath}`,
+    `${pkgPath}/src`,
+    `--out-dir ${pkgPath}/lib`,
+    `--extensions "${babelExt}"`
+  ].join(' ');
+
+  await asyncExec(`cross-env NODE_ENV=lib babel ${babelCommandStack}`, { silent: true });
+};
+
+/**
+ * @param {string} pkgPath
+ * @return {Promise<void>}
+ */
+const processOtherFiles = async (pkgPath) => {
   const pattern = path.join(pkgPath, 'src', '**', '*.*');
   const files = glob.sync(pattern, { ignore: [`**/*.{${['vue', ...scriptExtensions].join(',')}}`] });
+
+  const operations = [];
+
   for (const file of files) {
-    try {
-      fs.copySync(file, changePathToLib(pkgPath, file));
-    } catch (err) {
-      console.info(err);
-    }
+    operations.push(fs.copy(file, changePathToLib(pkgPath, file)));
+  }
+
+  try {
+    await Promise.all(operations);
+  } catch (err) {
+    console.info(err);
   }
 };
 
 /**
- * @param {string} pkgPath
+ *
+ * @param pkgPath
+ * @return {Promise<string>}
  */
-export const buildPackageLib = (pkgPath) => {
-  spinner.start(`Building: ${pkgPath}`);
-  processVue(pkgPath);
-  processOtherFiles(pkgPath);
-  processScripts(pkgPath);
-  spinner.succeed(`Complete: ${pkgPath}`);
+export const buildPackageLib = async (pkgPath) => {
+  const { name } = await fs.readJson(`${pkgPath}/package.json`);
+
+  await cleanLib(pkgPath);
+  await Promise.all([
+    processVue(pkgPath),
+    processScripts(pkgPath),
+    generateTsDefinitions(pkgPath),
+    processOtherFiles(pkgPath)
+  ]);
+  await lintLib(pkgPath);
+
+  return name;
 };
