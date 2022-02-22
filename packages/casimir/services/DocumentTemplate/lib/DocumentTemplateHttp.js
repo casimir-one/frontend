b323781c3e34b9ece56c9d596ea4c95bcd631eb8
Stacey Korbit <nastyasworld@gmail.com>
2022-02-22 15:41:13 +0300

    docs(@deip/document-template-service): add jsdoc

--- packages/casimir/services/DocumentTemplate/lib/DocumentTemplateHttp.js
+++ packages/casimir/services/DocumentTemplate/lib/DocumentTemplateHttp.js
@@ -4,22 +4,47 @@ import { createInstanceGetter } from '@deip/toolbox';
 export class DocumentTemplateHttp {
   http = HttpService.getInstance();
 
+  /**
+   * Get document template by id
+   * @param {string} id
+   * @returns {Promise<Object>}
+   */
   async getOne(id) {
     return this.http.get(`/api/v2/document-template/${id}`);
   }
 
+  /**
+   * Get document template list by team or user account
+   * @param {string} account
+   * @returns {Promise<Object>}
+   */
   async getListByAccount(account) {
     return this.http.get(`/api/v2/document-templates/account/${account}`);
   }
 
+  /**
+   * Create document template
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async create(req) {
     return this.http.post('/api/v2/document-template', req.getHttpBody());
   }
 
+  /**
+   * Update document template
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async update(req) {
     return this.http.put('/api/v2/document-template', req.getHttpBody());
   }
 
+  /**
+   * Delete document template
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async delete(req) {
     return this.http.put('/api/v2/document-template/delete', req.getHttpBody());
   }
--- packages/casimir/services/DocumentTemplate/lib/DocumentTemplateService.js
+++ packages/casimir/services/DocumentTemplate/lib/DocumentTemplateService.js
@@ -10,26 +10,57 @@ import { DocumentTemplateHttp } from './DocumentTemplateHttp';
 export class DocumentTemplateService {
   documentTemplateHttp = DocumentTemplateHttp.getInstance();
 
+  /**
+   * Get document template by id
+   * @param {string} id
+   * @returns {Promise<Object>}
+   */
   async getOne(id) {
     return this.documentTemplateHttp.getOne(id);
   }
 
+  /**
+   * Get document template list by user or team account
+   * @param {string} account
+   * @returns {Promise<Object>}
+   */
   async getListByAccount(account) {
     return this.documentTemplateHttp.getListByAccount(account);
   }
 
+  /**
+   * Create document template
+   * @param {Object} payload
+   * @param {string} payload.account
+   * @param {string} payload.title
+   * @param {Object} payload.body
+   * @returns {Promise<Object>}
+   */
   async create(payload) {
     const createDocumentTemplateCmd = new CreateDocumentTemplateCmd(payload);
     const msg = new JsonDataMsg({ appCmds: [createDocumentTemplateCmd] });
     return this.documentTemplateHttp.create(msg);
   }
 
+  /**
+   * Update document template
+   * @param {Object} payload
+   * @param {string} payload._id
+   * @param {string} payload.title
+   * @param {Object} payload.body
+   * @returns {Promise<Object>}
+   */
   async update(payload) {
     const updateDocumentTemplateCmd = new UpdateDocumentTemplateCmd(payload);
     const msg = new JsonDataMsg({ appCmds: [updateDocumentTemplateCmd] });
     return this.documentTemplateHttp.update(msg);
   }
 
+  /**
+   * Delete document template
+   * @param {string} id
+   * @returns {Promise<Object>}
+   */
   async delete(id) {
     const deleteDocumentTemplateCmd = new DeleteDocumentTemplateCmd({ documentTemplateId: id });
     const msg = new JsonDataMsg({ appCmds: [deleteDocumentTemplateCmd] });
