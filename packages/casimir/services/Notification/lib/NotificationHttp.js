60fe786c60ea4e76fb996ef93a519256111524e2
Stacey Korbit <nastyasworld@gmail.com>
2022-02-22 17:01:34 +0300

    docs(@deip/notification-service, @deip/portal-service): add jsdoc

--- packages/casimir/services/Notification/lib/NotificationHttp.js
+++ packages/casimir/services/Notification/lib/NotificationHttp.js
@@ -1,13 +1,26 @@
 import { HttpService } from '@deip/http-service';
 import { createInstanceGetter } from '@deip/toolbox';
 
+/**
+ * Notification HTTP transport
+ */
 export class NotificationHttp {
   http = HttpService.getInstance();
 
+  /**
+   * Get notifications by username
+   * @param {string} username
+   * @returns {Promise<Object>}
+   */
   async getListByUser(username) {
     return this.http.get(`/api/v2/notifications/user/${username}`);
   }
 
+  /**
+   * Mark user notifications as read
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async markUserNotificationsAsRead(req) {
     return this.http.put('/api/v2/notifications/mark-read', req.getHttpBody());
   }
--- packages/casimir/services/Portal/lib/PortalHttp.js
+++ packages/casimir/services/Portal/lib/PortalHttp.js
@@ -1,45 +1,95 @@
 import { HttpService } from '@deip/http-service';
 import { createInstanceGetter } from '@deip/toolbox';
 
+/**
+ * Portal HTTP transport
+ */
 export class PortalHttp {
   http = HttpService.getInstance();
 
+  /**
+   * Get portal
+   * @returns {Promise<Object>}
+   */
   async getPortal() {
     return this.http.get('/portal');
   }
 
+  /**
+   * Get network portal by portal id
+   * @param {string} portalId
+   * @returns {Promise<Object>}
+   */
   async getNetworkPortal(portalId) {
     return this.http.get(`/api/network/portals/${portalId}`);
   }
 
+  /**
+   * Get network portal list
+   * @returns {Promise<Object>}
+   */
   async getNetworkPortals() {
     return this.http.get('/api/network/portals/listing');
   }
 
+  /**
+   * Update portal profile
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async updatePortalProfile(req) {
     return this.http.put('/portal/profile', req.getHttpBody());
   }
 
+  /**
+   * Update portal network settings
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async updateNetworkSettings(req) {
     return this.http.put('/portal/network-settings', req.getHttpBody());
   }
 
+  /**
+   * Update portal settings
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async updatePortalSettings(req) {
     return this.http.put('/portal/settings', req.getHttpBody(), { headers: req.getHttpHeaders() });
   }
 
+  /**
+   * Post sign up
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async postSignUp(req) {
     return this.http.post('/portal/v2/registry/sign-up', req.getHttpBody());
   }
 
+  /**
+   * Approve sign up request
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async approveSignUpRequest(req) {
     return this.http.post('/portal/v2/registry/sign-ups/approve', req.getHttpBody());
   }
 
+  /**
+   * Get sign up requests
+   * @returns {Promise<Object>}
+   */
   async getSignUpRequests() {
     return this.http.get('/portal/registry/sign-ups');
   }
 
+  /**
+   * Reject sign up request
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async rejectSignUpRequest(req) {
     return this.http.put('/portal/registry/sign-ups/reject', req.getHttpBody());
   }
