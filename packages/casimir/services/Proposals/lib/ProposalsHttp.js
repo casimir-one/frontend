3b3a1988103d0278b164a83f0f2b426e73ecb5a9
Elena Vlasova <e.vlasova@paralect.com>
2022-02-22 14:22:30 +0100

    refactor(@deip/proposals-service, @deip/contract-agreements-module): change methods payload and add doc

--- packages/casimir/services/Proposals/lib/ProposalsHttp.js
+++ packages/casimir/services/Proposals/lib/ProposalsHttp.js
@@ -1,21 +1,45 @@
 import { HttpService } from '@deip/http-service';
 import { createInstanceGetter } from '@deip/toolbox';
 
+/**
+ *  Proposals HTTP transport
+ */
 export class ProposalsHttp {
   http = HttpService.getInstance();
 
+  /**
+   * Accept proposal
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async accept(req) {
     return this.http.put('/api/v2/proposals/update', req.getHttpBody());
   }
 
+  /**
+   * Decline proposal
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async decline(req) {
     return this.http.put('/api/v2/proposals/decline', req.getHttpBody());
   }
 
+  /**
+   * Get proposals by account and status
+   * @param {string} account
+   * @param {number} status
+   * @returns {Promise<Object>}
+   */
   async getListByAccount(account, status) {
     return this.http.get(`/api/v2/proposals/${account}/${status}`);
   }
 
+  /**
+   * Get proposal
+   * @param {string} id
+   * @returns {Promise<Object>}
+   */
   async getOne(id) {
     return this.http.get(`/api/v2/proposals/${id}`);
   }
