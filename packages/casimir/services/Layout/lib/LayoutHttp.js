03f3266f739eda47146bc7a4d5e567f1f8db5363
Elena Vlasova <e.vlasova@paralect.com>
2022-02-22 10:22:54 +0100

    docs(@deip/layout-service): add jsdoc

--- packages/casimir/services/Layout/lib/LayoutHttp.js
+++ packages/casimir/services/Layout/lib/LayoutHttp.js
@@ -1,41 +1,77 @@
 import { HttpService } from '@deip/http-service';
 import { createInstanceGetter } from '@deip/toolbox';
 
+/**
+ * Layouts HTTP transport
+ */
 export class LayoutHttp {
   http = HttpService.getInstance();
 
+  /**
+   * Get layout
+   * @param {string} layoutId
+   * @returns {Promise<Object>}
+   */
   async getLayout(layoutId) {
     return this.http.get(`/api/v2/layout/${layoutId}`);
   }
 
+  /**
+   * Get layouts
+   * @returns {Promise<Object>}
+   */
   async getLayouts() {
     return this.http.get('/api/v2/layouts');
   }
 
+  /**
+   * Get layouts by scope
+   * @param {string} scope
+   * @returns {Promise<Object>}
+   */
   async getLayoutsByScope(scope) {
     return this.http.get(`/api/v2/layouts/scope/${scope}`);
   }
 
+  /**
+   * Create layout
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async createLayout(req) {
     return this.http.post('/api/v2/layout', req.getHttpBody());
   }
 
+  /**
+   * Update layout
+   * @param {Object} req
+   */
   async updateLayout(req) {
     return this.http.put('/api/v2/layout', req.getHttpBody());
   }
 
+  /**
+   * Delete layout
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async deleteLayout(req) {
     return this.http.put('/api/v2/layout/delete', req.getHttpBody());
   }
 
+  /**
+   * Get layouts settings
+   * @returns {Promise<Object>}
+   */
   async getSettings() {
     return this.http.get('/portal/settings/layout-settings');
   }
 
-  async updateLayouts(req) {
-    return this.http.put('/portal/settings/layouts', req.getHttpBody());
-  }
-
+  /**
+   * Update settings
+   * @param {Object} req
+   * @returns {Promise<Object>}
+   */
   async updateSettings(req) {
     return this.http.put('/portal/settings/layout-settings', req.getHttpBody());
   }
