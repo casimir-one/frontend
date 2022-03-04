984af0a1d992a3c1e77961559c85007f95d55702
Stacey Korbit <nastyasworld@gmail.com>
2022-03-04 15:13:53 +0300

    docs(@deip/proxydi): add jsdoc

--- packages/common/proxydi/lib/proxydi.js
+++ packages/common/proxydi/lib/proxydi.js
@@ -3,30 +3,57 @@ import { classSingleton } from '@deip/toolbox';
 export class Proxydi {
   _registry = {};
 
+  /**
+  * Get proxy
+  * @param {string} name
+  * @returns {*}
+*/
   get(name) {
     return this._registry[name];
   }
 
+  /**
+  * Register proxy
+  * @param {string} name
+  * @param {*} value
+*/
   register(name, value) {
     this._registry[name] = value;
   }
 
+  /**
+  * Batch register
+  * @param {Object} registry
+*/
   batchRegister(registry) {
     for (const [name, value] of Object.entries(registry)) {
       this.register(name, value);
     }
   }
 
+  /**
+  * Remove proxy
+  * @param {string} name
+*/
   remove(name) {
     delete this._registry[name];
   }
 
+  /**
+  * Batch remove
+  * @param {Array.<string>} names
+*/
   batchRemove(names) {
     for (const name of names) {
       this.remove(name);
     }
   }
 
+  /**
+  * Check for proxy
+  * @param {string} name
+  * @returns {boolean}
+*/
   has(name) {
     return !!this._registry[name];
   }
