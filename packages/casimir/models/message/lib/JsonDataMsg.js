97b293a379b3dadf22feb5495b69978d5e7a8c6e
Elena Vlasova <e.vlasova@paralect.com>
2022-03-12 16:24:04 +0100

    docs(@deip/messages): add docs

--- packages/casimir/models/message/lib/JsonDataMsg.js
+++ packages/casimir/models/message/lib/JsonDataMsg.js
@@ -1,19 +1,28 @@
 import BaseHttpMsg from './base/BaseHttpMsg';
 
-
+/**
+ * JSON data message
+ * @extends BaseHttpMsg
+ */
 class JsonDataMsg extends BaseHttpMsg {
-
+  /**
+   * Create JSON data message
+   * @param {Object} payload
+   * @param {Object} headers
+   */
   constructor(payload, headers) {
     super(payload, headers);
     this._headers['content-type'] = 'application/json';
-    this._jsonData = { 'envelope': this._envelope.serialize() };
+    this._jsonData = { envelope: this._envelope.serialize() };
   }
 
+  /**
+   * Get HTTP request body
+   * @returns {Object}
+   */
   getHttpBody() {
     return this._jsonData;
   }
-
 }
-
 
 export default JsonDataMsg;
--- packages/casimir/models/message/lib/MultFormDataMsg.js
+++ packages/casimir/models/message/lib/MultFormDataMsg.js
@@ -1,8 +1,16 @@
 import BaseHttpMsg from './base/BaseHttpMsg';
 
-
+/**
+ * Multipart form data message
+ * @extends BaseHttpMsg
+ */
 class MultFormDataMsg extends BaseHttpMsg {
-
+/**
+ * Create multipart form data message
+ * @param {FoemData} formData
+ * @param {Object} payload
+ * @param {Object} headers
+ */
   constructor(formData, payload, headers) {
     super(payload, headers);
     this._headers['content-type'] = 'multipart/form-data';
@@ -10,11 +18,13 @@ class MultFormDataMsg extends BaseHttpMsg {
     this._formData.set('envelope', JSON.stringify(this._envelope.serialize()));
   }
 
-  getHttpBody() { 
-    return this._formData; 
+  /**
+   * Get HTTP request body
+   * @returns {Object}
+   */
+  getHttpBody() {
+    return this._formData;
   }
-
 }
-
 
 export default MultFormDataMsg;
