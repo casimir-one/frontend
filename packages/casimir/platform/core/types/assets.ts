b63222887b829a7cde51b0d2be9df7b42cf2cdf8
Euheny Bondarovich <euheny@gmail.com>
2022-04-01 09:35:42 +0300

    feat(@casimir/platform-core): add token types

--- packages/casimir/platform/core/types/assets.ts
+++ packages/casimir/platform/core/types/assets.ts
@@ -1,40 +1,5 @@
 import { ServiceBasePayload } from './servicePayload';
 
-// TODO: move transfer/exchange to tokens after service update
-
-/**
- * Transfer asset data
- */
-type AssetTransferData = {
-  from: string,
-  to: string
-  asset: Record<string, unknown>
-};
-
-/**
- * Transfer asset payload
- */
-export type AssetTransferPayload = ServiceBasePayload<AssetTransferData>;
-
-// //////////////
-
-/**
- * Exchange asset data
- */
-type AssetExchangeData = {
-  from: string,
-  to: string
-  fromAsset: Record<string, unknown>
-  toAsset: Record<string, unknown>
-};
-
-/**
- * Exchange asset payload
- */
-export type AssetExchangeProposalPayload = ServiceBasePayload<AssetExchangeData>;
-
-// //////////////
-
 /**
  * Deposit asset data
  */
