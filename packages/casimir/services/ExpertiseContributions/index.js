b6cbb25a83c597deae2eee10e81f3fa9a158e7b0
Euheny Bondarovich <euheny@gmail.com>
2022-06-03 16:39:37 +0300

    chore: remove unused services

--- packages/casimir/services/ExpertiseContributions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './lib/ExpertiseContributionsService';
-export {
-  EXPERTISE_CONTRIBUTION_TYPE,
-  ECI_STAT_PERIOD_STEP_TYPE
-} from '@deip/constants';
--- packages/casimir/services/Search/lib/SearchHttp.js
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@deip/http-service';
-import { makeSingletonInstance } from '@deip/toolbox';
-
-export class SearchHttp {
-  http = HttpService.getInstance();
-
-  /** @type {() => SearchHttp} */
-  static getInstance = makeSingletonInstance(() => new SearchHttp());
-}
