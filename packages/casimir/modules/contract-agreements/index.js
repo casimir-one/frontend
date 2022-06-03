4bff643ad7a599ffe5c0ad67897e02cd6d99ecfd
Euheny Bondarovich <euheny@gmail.com>
2022-06-03 17:42:20 +0300

    chore: remove unused packages

--- packages/casimir/modules/contract-agreements/index.js
+++
@@ -1,5 +0,0 @@
-export * from './lib/contractAgreementsModule';
-
-export { default as ContractAgreementsDataProvider } from './lib/components/DataProvider/ContractAgreementsDataProvider';
-export { default as ContractsTable } from './lib/components/Table/ContractsTable';
-export { default as ContractPdfDetails } from './lib/components/Details/ContractPdfDetails';
--- packages/casimir/modules/investment-opportunities/index.js
+++
@@ -1,8 +0,0 @@
-export * from './lib/investmentOpportunitiesModule';
-
-export { default as CreateCrowdfundingForm } from './lib/components/crowdfunding/Create/CreateCrowdfundingForm';
-export { default as InvestForm } from './lib/components/crowdfunding/Invest/InvestForm';
-export { default as CrowdfundingProgress } from './lib/components/crowdfunding/Progress/CrowdfundingProgress';
-export { default as CrowdfundingWidget } from './lib/components/crowdfunding/Widget/CrowdfundingWidget';
-
-export * from './lib/blocks';
