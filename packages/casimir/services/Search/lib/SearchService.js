e9e59dfa81e09319af0dd1d9aa2a3ea4a17dcca6
Euheny Bondarovich <euheny@gmail.com>
2022-06-03 16:39:37 +0300

    chore: remove unused services

--- packages/casimir/services/Search/lib/SearchService.js
+++
@@ -1,9 +0,0 @@
-import { makeSingletonInstance } from '@deip/toolbox';
-import { SearchHttp } from './SearchHttp';
-
-export class SearchService {
-  searchHttp = SearchHttp.getInstance();
-
-  /** @type {() => SearchService} */
-  static getInstance = makeSingletonInstance(() => new SearchService());
-}
