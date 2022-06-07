c4bd27b3baa78a7800401424b7de7a3bc404fb2a
Elena Vlasova <e.vlasova@paralect.com>
2022-06-07 11:01:29 +0200

    chore(@deip/toolbox): small change to publish package

--- packages/common/toolbox/src/utils/singleton.js
+++ packages/common/toolbox/src/utils/singleton.js
@@ -1,10 +1,10 @@
 export class Singleton {
   static instance;
   /**
- * Get the Singleton instance.
- * @param {...any} params
- * @returns {Singleton}
- */
+   * Get the Singleton instance.
+   * @param {...any} params
+   * @returns {Singleton}
+   */
   static getInstance(...params) {
     // eslint-disable-next-line no-return-assign
     return this.instance ? this.instance : this.instance = new this(...params);
