4c32e7282799900967c4cf3ce0dd20e792005d42
Elena Vlasova <e.vlasova@paralect.com>
2022-07-09 00:42:37 +0200

    feat(@deip/attributes-module): add attributes to module options

--- packages/casimir/modules/attributes/lib/attributesModule.js
+++ packages/casimir/modules/attributes/lib/attributesModule.js
@@ -6,7 +6,10 @@ const install = (Vue, options = {}) => {
   if (install.installed) return;
   install.installed = true;
 
-  const { store } = options;
+  const {
+    store,
+    attributes = []
+  } = options;
 
   if (store) {
     store.registerModule('attributesRegistry', attributesRegistry);
@@ -14,6 +17,10 @@ const install = (Vue, options = {}) => {
     store.registerModule('attributes', attributesStore);
     store.dispatch('attributes/getList');
     store.dispatch('attributes/getSettings');
+
+    for (const attribute of attributes) {
+      store.dispatch('attributesRegistry/addAttribute', attribute);
+    }
 
     Object.defineProperty(Vue.prototype, '$attributes', {
       get() {
