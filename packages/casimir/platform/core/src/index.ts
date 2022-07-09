2712670adfc65fa520481be7106d15060b3e2ec1
Euheny Bondarovich <euheny@gmail.com>
2022-07-09 11:47:27 +0300

    fix(@deip/platform-core): ts building

--- packages/casimir/platform/core/src/index.ts
+++ packages/casimir/platform/core/src/index.ts
@@ -1,1 +1,24 @@
 export * from './constants';
+
+export type {
+  ServiceBasePayload,
+  ServicePayloadInitiator,
+  ServicePayloadProposalInfo,
+
+  AssetDepositPayload,
+
+  AttributeTypes,
+  AttributeValueTypes,
+  AttributeComponent,
+  AttributeTypeInfo,
+
+  FungibleTokenCreateData,
+  FungibleTokenIssueData,
+  FungibleTokenCreatePayload,
+  FungibleTokenIssuePayload,
+
+  NonFungibleTokenCreateData,
+  NonFungibleTokenIssueData,
+  NonFungibleTokenCreatePayload,
+  NonFungibleTokenIssuePayload
+} from './types';
