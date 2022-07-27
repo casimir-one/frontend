6d05cffec8c8d869f87e894c85d5e07dbaf16328
Elena Vlasova <e.vlasova@paralect.com>
2022-07-27 14:14:26 +0200

    feat(@deip/messages): CAS-36 remove FinalizedTxMsg

--- packages/casimir/models/message/index.js
+++ packages/casimir/models/message/index.js
@@ -1,9 +1,7 @@
 import JsonDataMsg from './lib/JsonDataMsg';
 import MultFormDataMsg from './lib/MultFormDataMsg';
-import FinalizedTxMsg from './lib/FinalizedTxMsg';
 
 export {
   JsonDataMsg,
-  MultFormDataMsg,
-  FinalizedTxMsg
+  MultFormDataMsg
 };
