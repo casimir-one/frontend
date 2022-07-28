8a0b0311561218f5f7a17a6e60c52a1bbd4a5b28
Igor Novitsky <ionovitsky@gmail.com>
2022-07-28 12:23:26 +0300

    feat(@deip/assets-module, @casimir/token-service): CAS-156 Get balance by address

--- packages/casimir/services/token/src/services/fungible/FungibleTokenService.js
+++ packages/casimir/services/token/src/services/fungible/FungibleTokenService.js
@@ -203,27 +203,17 @@ export class FungibleTokenService {
 
   /**
    * Get dao balance for certain account
-   * @param {string} owner
+   * @param {string} address
    * @return {Promise<object>}
    */
-  async getAccountDaoBalance(owner) {
+  async getAccountDaoBalance(address) {
     const env = this.proxydi.get('env');
 
-    return ChainService.getInstanceAsync(env).then((chainService) => {
-      const chainRpc = chainService.getChainRpc();
-      const api = chainService.getChainNodeClient();
+    const chainService = await ChainService.getInstanceAsync(env);
+    const chainRpc = chainService.getChainRpc();
 
-      return api.query.deipDao.daoRepository(`0x${owner}`).then((daoOpt) => {
-        if (daoOpt.isSome) {
-          const walletAddress = daoOpt.value.toJSON().authority.signatories[0];
-          return chainRpc
-            .getFungibleTokenBalanceByOwnerAsync(walletAddress, 0)
-            .then((balance) => balance);
-        }
-
-        return null;
-      });
-    });
+    return chainRpc
+      .getFungibleTokenBalanceByOwnerAsync(address, 0);
   }
 
   /**
