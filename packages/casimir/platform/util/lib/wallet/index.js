f612944432cfe3128ce3c3bd0878d5905112bbab
Elena Vlasova <e.vlasova@paralect.com>
2022-08-09 15:17:45 +0200

    fix(@deip/platform-util): open wallet sign transaction window as popup

--- packages/casimir/platform/util/lib/wallet/index.js
+++ packages/casimir/platform/util/lib/wallet/index.js
@@ -1,10 +1,29 @@
 import { proxydi } from '@deip/proxydi';
 
+const getWindowOptions = () => {
+  const left = (window.screen.width - 400) / 2;
+  const top = (window.screen.height - 640) / 2;
+
+  const options = {
+    popup: true,
+    width: 420,
+    height: 600,
+    menubar: 'no',
+    toolbar: 'no',
+
+    top,
+    left
+  };
+
+  return Object.keys(options)
+    .reduce((acc, key) => `${acc},${key}=${options[key]}`, '');
+};
+
 export const walletSignTx = async (packedTx, chainInfo) => {
   const { WALLET_URL } = proxydi.get('env');
   const { TxClass, metadata } = chainInfo;
 
-  const wallet = window.open(`${WALLET_URL}/sign-transaction`, '_blank');
+  const wallet = window.open(`${WALLET_URL}/sign-transaction`, '_blank', getWindowOptions());
 
   const signedTx = await new Promise((resolve, reject) => {
     const handleSignTx = (event) => {
